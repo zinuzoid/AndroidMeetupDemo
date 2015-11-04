97087cd11de9b604be2d981ee7e617959c786fc9
Jetsada Machom <zinuzoid@gmail.com>
2015-11-04 16:56:46 +0700

    pass commit

--- app/src/main/java/com/zinuzoid/productivityandroiddemo/ui/MainActivity.java
+++ app/src/main/java/com/zinuzoid/productivityandroiddemo/ui/MainActivity.java
@@ -14,6 +14,7 @@ import com.zinuzoid.productivityandroiddemo.R;
 public class MainActivity extends AppCompatActivity {
 
 	int a;
+	int b;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {

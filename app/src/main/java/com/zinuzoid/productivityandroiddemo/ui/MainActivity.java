974b409ba2cf8d370abd60807d000a9272557825
Jetsada Machom <zinuzoid@gmail.com>
2015-11-04 16:45:51 +0700

    update

--- app/src/main/java/com/zinuzoid/productivityandroiddemo/ui/MainActivity.java
+++ app/src/main/java/com/zinuzoid/productivityandroiddemo/ui/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -36,20 +35,9 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
 		if(id == R.id.action_settings) {
 			a++;
 			Toast.makeText(MainActivity.this, "Toast " + String.valueOf(a), Toast.LENGTH_SHORT).show();
@@ -57,14 +45,6 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-
-	public void A() {
-		int a = ten() + 5;
-	}
-
-	public int ten() {
-		return 10;
 	}
 
 }
